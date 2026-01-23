70a88fa476af775ed2771843f3b27ae9b9d82be1
ChingHui418 <a0985218998@gmail.com>
2026-01-23 13:52:15 +0800

    20260123-Tomcat1

---
+++ src/main/java/tw/hui/controller/GiftServlet.java
@@ -0,0 +1,50 @@
+package tw.hui.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tw.hui.apis.Gift;
+import tw.hui.dao.GiftDAO;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/GiftMain")
+public class GiftServlet extends HttpServlet {
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. User
+		request.setCharacterEncoding("UTF-8");
+		int page = 1;
+		try {
+			page = Integer.parseInt(request.getParameter("page"));
+		}catch (Exception e) {
+			
+		}
+		
+		// 2. Model
+		try {
+			List<Gift> gifts = new GiftDAO(page, 10).findAll();
+//			if(gifts == null) {
+//				System.out.println(gifts.size());
+//			}else {
+//				System.out.println("gift null");
+//			}
+			
+			request.setAttribute("gifts", gifts);
+			request.setAttribute("prev", page - 1);
+			request.setAttribute("page", page);
+			request.setAttribute("next", page + 1);
+			
+//			System.out.println(gifts.size());
+//			System.out.println(gifts.get(0).getName());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		// 3. Viewer
+		request.getRequestDispatcher("GiftViewer").forward(request, response);
+	}
+
+}
---
+++ src/main/java/tw/hui/listener/InitListener.java
@@ -0,0 +1,28 @@
+package tw.hui.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class InitListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("Start");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("MySQL Driver Load Success");
+		} catch (ClassNotFoundException e) {
+			System.out.println("MySQL Driver ERROP");
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("End");
+	}
+
+	
+}
